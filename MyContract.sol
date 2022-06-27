7954e72432b8085134f48aa68bfedc5792f79211
zeredogo <zeredogo@gmail.com>
2022-06-27 06:44:20 -0700

    updating commit

---
+++ MyContract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+  // Arrayss
+  uint[] public uintArray = [1, 2, 3];
+  string[] public stringArray = [ 'apple', 'banana', 'carrot'];
+  string[] public values;
+  uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
+
+  function addValue(string memory _value) public{
+    values.push(_value);
+  }
+
+  function valueCount() public  view returns(uint) {
+    return values.length;
+  }
+}
