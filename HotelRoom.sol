d62f7f33a46da45aaa9278664b19a7d97c9805da
zeredogo <zeredogo@gmail.com>
2022-07-01 16:26:32 -0700

    more examples

---
+++ HotelRoom.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom {
+    // Vacant
+    //Occupied
+
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+
+    Statuses currentStatus;
+
+    event Occupy(address _occupant, uint _value);
+
+    address payable public owner;
+
+    constructor() {
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+          require(currentStatus == Statuses.Vacant, 'Currently occupied.');
+          _;
+    }
+
+    modifier costs(uint _amount) {
+         require(msg.value >= 2 ether, 'Not enough ether provided.');
+         _;
+    }
+ 
+    function book() payable onlyWhileVacant {
+      currentStatus = Statuses.Occupied;
+      owner.transfer(msg.value);
+      (bool sent, bytes memory data) = owner.call{value: msg.value}('');
+      require(true);
+
+      emit Occupy(msg.sender, msg.value);
+    }
+
+}
